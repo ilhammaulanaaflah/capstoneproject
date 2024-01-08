aa0a455c776de70dc16b3238eff5e9e756354155
ilhammaulanaaflah <142953488+ilhammaulanaaflah@users.noreply.github.com>
2024-01-08 10:44:34 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+Penggunaan Web Poliklinik
+
+Database
+Database poliklinik tersedia pada folder database/poli.sql
+
+---
+
+Login Sebagai Admin
+
+username : Admin
+password : admin
+
+---
+
+Login Sebagai Dokter
+
+username : dr. Adhi Wicaksono, Sp.M
+password : 12345
+
+---
+
+Login Sebagai Pasien
+
+username : rafi
+password : 12345
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Poliklinik</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="hold-transition login-page">
+    <div class="container-fluid flex bg-sky-600 flex-col justify-center items-center text-white p-5"
+        style="height: 400px;">
+        <h1 class="font-weight-bold mb-3">Poliklinik</h1>
+        <h5>
+            "Sehat Terbaik Dimulai di Sini: Poliklinik Kami, Pelayanan Berkualitas untuk Kesehatan Optimal Anda!"</h5>
+    </div>
+    <div class="container mt-5">
+        <div class="row justify-content-lg-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                    <h1><i class="nav-icon fas fa-user bg-primary text-white p-3 rounded"></i></h1>
+                    <h2 class="font-weight-bold">Login Sebagai Pasien</h2>
+                    <p>Apabila anda adalah seorang pasien, silahkan login terlebih dahulu untuk melakukan pendaftaran sebagai pasien!</p>
+                    <a href="loginUser.php">Klik Link Berikut <i class="fas fa-arrow-right"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                    <h1><i class="nav-icon fas fa-user bg-primary text-white p-3 rounded"></i></h1>
+                    <h2 class="font-weight-bold">Login Sebagai Dokter</h2>
+                    <p>Apabila anda adalah seorang dokter, silahkan login terlebih dahulu untuk melakukan mulai melayani pasien!</p>
+                    <a href="login.php">Klik Link Berikut <i class="fas fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.login-box -->
+
+        <!-- jQuery -->
+        <script src="assets/plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="assets/dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
